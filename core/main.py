d05a0e1904714ad575ee47d6ed89f2dedaab81b5
Stanislav <vstasv05@yadnex.ru>
2025-11-21 12:20:34 +0700

    oop

--- core/main.py
+++ core/main.py
@@ -1,11 +1,9 @@
 from machine import Pin, SPI
 import st7789
+
 import time
 
-TFA = 40
-BFA = 40
-
-def config(rotation=0, buffer_size=0, options=0):
+def display_lilygo_config(rotation=0, buffer_size=0, options=0):
     return st7789.ST7789(
         SPI(2, baudrate=40000000, sck=Pin(18), mosi=Pin(19), miso=None),
         135,
@@ -16,22 +14,31 @@ def config(rotation=0, buffer_size=0, options=0):
         backlight=Pin(4, Pin.OUT),
         rotation=rotation,
         options=options,
-        buffer_size= buffer_size)
+        buffer_size=buffer_size
+    )
 
-tft = config()
+
+class ESP32:
+    def __init__(self, display_factory: callable):
+        self.display1 = display_factory()
+        self.display1.init()
+
+        self.left_btn = Pin(0, Pin.IN)
+        self.right_btn = Pin(35, Pin.IN)
+
+        try:
+            self.display1.backlight.on()
+        except:
+            pass
+
+    def turn(self):
+        self.display1.fill(st7789.RED)
+
 
 if __name__ == '__main__':
-    # обязательно инициализировать дисплей
-    tft.init()
+    esp = ESP32(display_lilygo_config)
 
-    # включение подсветки
-    try:
-        tft.backlight.on()
-    except:
-        pass
-
-    # экран должен стать зелёным
-    tft.text(st7789.GREEN)
+    esp.turn()
 
     while True:
         time.sleep(1)
--- core/readme.md
+++ core/readme.md
@@ -8,4 +8,18 @@
 ```mpremote connect COM5 fs put main.py```
 
 ### Посмотреть все файлы ###
-```mpremote fs ls```
+```mpremote connect COM5 fs ls```
+
+
+```
+(venv) PS C:\Users\vstas\OneDrive\Рабочий стол\7sem\esp32_car_dashboard> ampy --port COM7 ls
+/boot.py
+/test.txt
+(venv) PS C:\Users\vstas\OneDrive\Рабочий стол\7sem\esp32_car_dashboard> cd core
+(venv) PS C:\Users\vstas\OneDrive\Рабочий стол\7sem\esp32_car_dashboard\core> ampy --port COM7 put main.py
+(venv) PS C:\Users\vstas\OneDrive\Рабочий стол\7sem\esp32_car_dashboard\core> ampy --port COM7 ls
+/boot.py
+/main.py
+/test.txt
+(venv) PS C:\Users\vstas\OneDrive\Рабочий стол\7sem\esp32_car_dashboard\core> mpremote connect COM7
+```
